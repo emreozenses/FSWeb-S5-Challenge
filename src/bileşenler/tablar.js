7ed49394ed4ff9b78d54b09a72ef06460b33c9a6
Emre OZENSES <emreozenses@gmail.com>
2023-10-23 21:12:18 +0300

    task 3 completed

--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,19 +1,31 @@
 const Tablar = (konu) => {
-  // GÖREV 3
-  // ---------------------
-  // Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
-  // Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
-  // fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
-  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">teknoloji</div>
-  // </div>
-  //
-}
+  const topicsDiv = document.createElement("div");
+  topicsDiv.className = "topics";
+
+  konu.forEach((topic) => {
+    const tabDiv = document.createElement("div");
+    tabDiv.className = "tab";
+    tabDiv.textContent = topic;
+    topicsDiv.append(tabDiv);
+  });
+
+  return topicsDiv;
+};
+
+// GÖREV 3
+// ---------------------
+// Tek argümanı bir dizi ("konu") olan bu fonksiyonu uygulayın.
+// Örnek olarak, konu dizisi şu şekilde deklare edilmişse ['javascript', 'bootstrap', 'teknoloji']
+// fonksiyon aşağıdaki şekilde bir DOM öğesi döndürecek.
+// Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+//
+// <div class="topics">
+//   <div class="tab">javascript</div>
+//   <div class="tab">bootstrap</div>
+//   <div class="tab">teknoloji</div>
+// </div>
+//
 
 const tabEkleyici = (secici) => {
   // GÖREV 4
@@ -23,6 +35,6 @@ const tabEkleyici = (secici) => {
   // Yanıtın içindeki konu dizisini bulun ve Tablar bileşenini kullanarak tabları oluşturun.
   // Tabları, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
   //
-}
+};
 
-export { Tablar, tabEkleyici }
+export { Tablar, tabEkleyici };
