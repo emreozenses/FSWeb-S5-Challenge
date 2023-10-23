566621643766e929fc973219961e7aff38d2f55f
Emre OZENSES <emreozenses@gmail.com>
2023-10-23 16:59:28 +0300

    task 1 completed

--- src/bileşenler/header.js
+++ src/bileşenler/header.js
@@ -1,17 +1,36 @@
 const Header = (baslik, tarih, yazi) => {
-  // GÖREV 1
-  // ---------------------
-  // Bu fonksiyon argüman olarak `baslik`, `tarih` ve `temp` alarak aşağıdaki yapıyı döndürecek.
-  // Kullanılan html etiketleri, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  //
-  //  <div class="header">
-  //    <span class="date">{ tarih }</span>
-  //    <h1>{ baslik }</h1>
-  //    <span class="temp">{ yazi }</span>
-  //  </div>
-  //
-}
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("header");
+
+  const dateSpan = document.createElement("span");
+  dateSpan.className = "date";
+  dateSpan.textContent = `${tarih}`;
+
+  const baslikH1 = document.createElement("h1");
+  baslikH1.textContent = `${baslik}`;
+
+  const tempSpan = document.createElement("span");
+  tempSpan.className = "temp";
+  tempSpan.textContent = `${yazi}`;
+
+  headerDiv.append(dateSpan);
+  headerDiv.append(baslik);
+  headerDiv.append(tempSpan);
+
+  return headerDiv;
+};
+// GÖREV 1
+// ---------------------
+// Bu fonksiyon argüman olarak `baslik`, `tarih` ve `temp` alarak aşağıdaki yapıyı döndürecek.
+// Kullanılan html etiketleri, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+//
+//  <div class="header">
+//    <span class="date">{ tarih }</span>
+//    <h1>{ baslik }</h1>
+//    <span class="temp">{ yazi }</span>
+//  </div>
+//
 
 const headerEkleyici = (secici) => {
   // GÖREV 2
@@ -20,10 +39,8 @@ const headerEkleyici = (secici) => {
   // Görev 1'de tanımladığınız Header bileşenini kullanarak bir header oluşturun (baslik,tarih,yazi parametrelerini kendi isteğinize göre belirleyin)
   // Oluşturulan header'i, verilen seçiciyle eşleşen DOM'daki öğeye eklemelidir.
   //
-
-  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper")) 
+  // İPUCU: querySelector bir string alabilir (bknz: querySelector("#wrapper"))
   // fakat aynı zamanda bir değişken de alabilir (bknz: querySelector(secici))
- 
-}
+};
 
-export { Header, headerEkleyici }
+export { Header, headerEkleyici };
