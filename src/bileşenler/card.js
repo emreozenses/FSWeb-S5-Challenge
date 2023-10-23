72f683fa463372de1d20a31db1f2ad60fe3b547b
Emre OZENSES <emreozenses@gmail.com>
2023-10-23 23:25:58 +0300

    task 6 completed

--- src/bileşenler/card.js
+++ src/bileşenler/card.js
@@ -1,10 +1,12 @@
-const Card = (makale) => {
+import axios from "axios";
+
+const Card = (makaleler) => {
   const cardDiv = document.createElement("div");
   cardDiv.className = "card";
 
   const headlineDiv = document.createElement("div");
   headlineDiv.className = "headline";
-  headlineDiv.textContent = makale.anabaslik;
+  headlineDiv.textContent = makaleler.anabaslik;
 
   const authorDiv = document.createElement("div");
   authorDiv.className = "author";
@@ -13,10 +15,10 @@ const Card = (makale) => {
   imgContainer.className = "img-container";
 
   const imgYazarFoto = document.createElement("img");
-  imgYazarFoto.src = makale.yazarFoto;
+  imgYazarFoto.src = makaleler.yazarFoto;
 
   const spanYazarAdi = document.createElement("span");
-  spanYazarAdi.textContent = `${makale.yazarAdi} + tarafından`;
+  spanYazarAdi.textContent = `${makaleler.yazarAdi} + tarafından`;
 
   imgContainer.append(imgYazarFoto);
   authorDiv.append(imgContainer, spanYazarAdi);
@@ -24,6 +26,10 @@ const Card = (makale) => {
 
   return cardDiv;
 };
+
+/*document.querySelector(".card").addEventListener("click", () => {
+  console.log(document.querySelector(".headline"));
+});*/
 
 // GÖREV 5
 // ---------------------
@@ -45,14 +51,49 @@ const Card = (makale) => {
 //
 
 const cardEkleyici = (secici) => {
-  // GÖREV 6
-  // ---------------------
-  // Tek bağımsız değişkeni olarak bir css seçici alan bu fonksiyonu uygulayın.
-  // Makaleleri bu uç noktadan almalıdır: `http://localhost:5001/api/makaleler` (console.log ile test edin!!).
-  // Bununla birlikte, makaleler tek bir düzenli dizi halinde organize edilmemiştir. Yanıtı yakından inceleyin!
-  // Card bileşenini kullanarak yanıttaki her makale nesnesinden bir kart oluşturun.
-  // Her cardı, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
-  //
+  const url = `http://localhost:5001/api/makaleler`;
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data.makaleler);
+      const bootstrap = response.data.makaleler.bootstrap;
+      bootstrap.forEach((element) => {
+        const newCard = Card(element);
+        document.querySelector(secici).append(newCard);
+      });
+      const javascript = response.data.makaleler.javascript;
+      javascript.forEach((element) => {
+        const newCard = Card(element);
+        document.querySelector(secici).append(newCard);
+      });
+      const jquery = response.data.makaleler.jquery;
+      jquery.forEach((element) => {
+        const newCard = Card(element);
+        document.querySelector(secici).append(newCard);
+      });
+      const nodejs = response.data.makaleler["node.js"];
+      nodejs.forEach((element) => {
+        const newCard = Card(element);
+        document.querySelector(secici).append(newCard);
+      });
+      const teknoloji = response.data.makaleler.teknoloji;
+      teknoloji.forEach((element) => {
+        const newCard = Card(element);
+        document.querySelector(secici).append(newCard);
+      });
+    })
+    .catch((err) => {
+      console.log("yükleme yapılamadı!" + err);
+    });
 };
+
+// GÖREV 6
+// ---------------------
+// Tek bağımsız değişkeni olarak bir css seçici alan bu fonksiyonu uygulayın.
+// Makaleleri bu uç noktadan almalıdır: `http://localhost:5001/api/makaleler` (console.log ile test edin!!).
+// Bununla birlikte, makaleler tek bir düzenli dizi halinde organize edilmemiştir. Yanıtı yakından inceleyin!
+// Card bileşenini kullanarak yanıttaki her makale nesnesinden bir kart oluşturun.
+// Her cardı, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
+//
 
 export { Card, cardEkleyici };
--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const Tablar = (data) => {
   const topicsDiv = document.createElement("div");
   topicsDiv.className = "topics";
-  const konuArr = data.konular;
-  konuArr.forEach((topic) => {
+  const konu = data.konular;
+  konu.forEach((topic) => {
     const tabDiv = document.createElement("div");
     tabDiv.className = "tab";
     tabDiv.textContent = topic;
