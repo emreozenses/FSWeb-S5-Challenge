a0ae284bd5cd417486dc19cf51e531ad7c7742d5
Emre OZENSES <emreozenses@gmail.com>
2023-10-23 22:31:58 +0300

    task 5 completed

--- src/bileşenler/card.js
+++ src/bileşenler/card.js
@@ -1,23 +1,48 @@
 const Card = (makale) => {
-  // GÖREV 5
-  // ---------------------
-  // Aşağıda gördüğünüz işaretlemeyi döndürmesi gereken bu fonksiyonu uygulayın.
-  // Tek argümanı olarak "anabaslik", "yazarFoto" ve "yazarAdı" özelliklerine sahip bir "makale" nesnesi alır.
-  // Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
-  // Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
-  // Bir kullanıcı bir kartı tıkladığında makalenin başlığının konsola kaydedilmesi için click event dinleyicisi ekleyin.
-  //
-  // <div class="card">
-  //   <div class="headline">{ anabaslik }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ yazarFoto }>
-  //     </div>
-  //     <span>{ yazarAdı } tarafından</span>
-  //   </div>
-  // </div>
-  //
-}
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+
+  const headlineDiv = document.createElement("div");
+  headlineDiv.className = "headline";
+  headlineDiv.textContent = makale.anabaslik;
+
+  const authorDiv = document.createElement("div");
+  authorDiv.className = "author";
+
+  const imgContainer = document.createElement("div");
+  imgContainer.className = "img-container";
+
+  const imgYazarFoto = document.createElement("img");
+  imgYazarFoto.src = makale.yazarFoto;
+
+  const spanYazarAdi = document.createElement("span");
+  spanYazarAdi.textContent = `${makale.yazarAdi} + tarafından`;
+
+  imgContainer.append(imgYazarFoto);
+  authorDiv.append(imgContainer, spanYazarAdi);
+  cardDiv.append(headlineDiv, authorDiv);
+
+  return cardDiv;
+};
+
+// GÖREV 5
+// ---------------------
+// Aşağıda gördüğünüz işaretlemeyi döndürmesi gereken bu fonksiyonu uygulayın.
+// Tek argümanı olarak "anabaslik", "yazarFoto" ve "yazarAdı" özelliklerine sahip bir "makale" nesnesi alır.
+// Kullanılan etiketler, öğelerin hiyerarşisi ve öznitelikleri sağlanan işaretlemeyle tam olarak eşleşmelidir!
+// Öğelerin içindeki metin, "textContent" özelliği kullanılarak ayarlanacaktır ("innerText" DEĞİL).
+// Bir kullanıcı bir kartı tıkladığında makalenin başlığının konsola kaydedilmesi için click event dinleyicisi ekleyin.
+//
+// <div class="card">
+//   <div class="headline">{ anabaslik }</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={ yazarFoto }>
+//     </div>
+//     <span>{ yazarAdı } tarafından</span>
+//   </div>
+// </div>
+//
 
 const cardEkleyici = (secici) => {
   // GÖREV 6
@@ -28,6 +53,6 @@ const cardEkleyici = (secici) => {
   // Card bileşenini kullanarak yanıttaki her makale nesnesinden bir kart oluşturun.
   // Her cardı, fonksiyona iletilen seçiciyle eşleşen DOM'daki öğeye ekleyin.
   //
-}
+};
 
-export { Card, cardEkleyici }
+export { Card, cardEkleyici };
